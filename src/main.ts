08fc9747cf9c23361c3daa2e85e44ce7f0e9ea26
Cristian Alejandro Barajas Bolivar <cristianbarajasimasc@gmail.com>
2025-09-16 18:54:54 -0400

    feat: explicacion andaluz

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,10 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-import { ValidationPipe } from '@nestjs/common';
+import { ValidationPipe, Logger } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  const logger = new Logger('Main');
 
   app.useGlobalPipes(
     new ValidationPipe({
@@ -12,6 +13,11 @@ async function bootstrap() {
     }),
   );
 
-  await app.listen(process.env.PORT ?? 3000);
+  const port = process.env.PORT ?? 3000;
+
+  await app.listen(port, () => {
+    logger.log(`Running application on port ${port}`);
+  });
 }
+
 bootstrap();
