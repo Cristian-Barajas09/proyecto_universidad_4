37d3f0c0b6d0f85ab0712b86351c151320853868
Cristian Alejandro Barajas Bolivar <cristianbarajasimasc@gmail.com>
2025-09-18 22:23:44 -0400

    feat: explain to frank

--- src/students/entities/student.entity.ts
+++ src/students/entities/student.entity.ts
@@ -1,10 +1,12 @@
-import { Schema, SchemaFactory } from '@nestjs/mongoose';
+import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
 import { Document, Types } from 'mongoose';
 
 @Schema()
 export class Student extends Document {
+  @Prop()
   public university: string;
 
+  @Prop({ type: Types.ObjectId, ref: 'User' })
   public user: Types.ObjectId;
 }
 
