7b7e65f6fb7d05c68a22950aea848afbbaee5bb9
Cristian Alejandro Barajas Bolivar <cristianbarajasimasc@gmail.com>
2025-10-04 03:11:27 -0400

    feat: add certifications

--- src/tutors/controllers/specialties.controller.ts
+++ src/tutors/controllers/specialties.controller.ts
@@ -1,11 +1,13 @@
 import { Controller, Get } from '@nestjs/common';
 import { SpecialtiesService } from '../services/specialties.service';
+import { Auth } from 'src/auth/decorators/auth.decorator';
 
 @Controller('specialties')
 export class SpecialtiesController {
   public constructor(private readonly specialtiesService: SpecialtiesService) {}
 
   @Get()
+  @Auth()
   public getSpecialties() {
     return this.specialtiesService.getSpecialties();
   }
